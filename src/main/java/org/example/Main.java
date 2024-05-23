085489830a14b5a6ddb9687d9a1668b617ed774c
Pinar <wi23b133@technikum-wien.at>
2024-05-23 16:14:13 +0200

    add greeting translation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Pinar!");
         GreetingTranslator greetingTranslator = new GreetingTranslator();
         String greeting = greetingTranslator.getGreeting(Locale.ENGLISH);
-
+        greeting += "Technikum";
+        System.out.println(greeting);
     }
 }
--- src/test/java/org/example/GreetingTranslatorTest.java
+++ src/test/java/org/example/GreetingTranslatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Locale;
 
+import static java.util.Locale.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTranslatorTest {
@@ -12,9 +13,25 @@ class GreetingTranslatorTest {
 void when_localGerman_then_GreetingHallo(){
     //Arrange
     GreetingTranslator greetingTranslator = new GreetingTranslator();
-    Locale locale = new Locale("en");
+    Locale locale = Locale.GERMAN;
     // Act
-
+String greeting = greetingTranslator.getGreeting(locale);
     // Assert
+        assertEquals("Hallo", greeting);
+}
+@Test
+    void when_localEnglish_then_GreetingHallo(){
+        GreetingTranslator greetingTranslator = new GreetingTranslator();
+        assertEquals("Hello", greetingTranslator.getGreeting(Locale.ENGLISH));
+}
+@Test
+    void when_localFrench_then_GreetingHallo(){
+        GreetingTranslator greetingTranslator = new GreetingTranslator();
+        assertEquals("Bonjour", greetingTranslator.getGreeting(Locale.FRENCH));
+}
+@Test
+    void when_localNotSupported_then_IllegalArgumentException(){
+        GreetingTranslator greetingTranslator = new GreetingTranslator();
+        assertThrows(IllegalArgumentException.class, () -> greetingTranslator.getGreeting(Locale.ITALIAN));
 }
 }
