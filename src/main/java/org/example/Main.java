da6e52d2d0b64493f112834445139173be565e57
Pinar <wi23b133@technikum-wien.at>
2024-06-02 18:21:03 +0200

    Game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
-import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        boolean playAgain;
+        Scanner scanner = new Scanner(System.in);
 
+        do {
+            game.start();
+            System.out.print("Möchten Sie erneut spielen? (ja/nein): ");
+            playAgain = scanner.next().equalsIgnoreCase("ja");
+        } while (playAgain);
+
+        scanner.close();
     }
 }
