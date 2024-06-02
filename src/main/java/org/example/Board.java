528bd4a295f2b50aee2a02d3fdbad2850707fc16
Pinar <wi23b133@technikum-wien.at>
2024-06-02 18:17:18 +0200

    Board2

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,2 +1,80 @@
-package org.example;public class Board {
+package org.example;
+
+public class Board {
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clear();
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == ' ';
+    }
+
+    public void place(int x, int y, char maker) {  //maker plazieren
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = maker;
+        }
+    }
+
+    public void clear() {   //Spielfeld leeren
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|" + cells[i][j]);
+            }
+            System.out.println("|");
+        }
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cells[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkForWin(char maker) {
+        return (checkRowsForWin(maker) || checkColumnsForWin(maker) || checkDiagonalsForWin(maker));
+    }
+
+    private boolean checkRowsForWin(char maker) {
+        for (int i = 0; i < 3; i++) {
+            if (cells[i][0] == maker && cells[i][1] == maker && cells[i][2] == maker) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkColumnsForWin(char maker) {
+        for (int i = 0; i < 3; i++) {
+            if (cells[0][i] == maker && cells[1][i] == maker && cells[2][i] == maker) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonalsForWin(char maker) { //
+        if (cells[0][0] == maker && cells[1][1] == maker && cells[2][2] == maker) {
+            return true;
+        }
+        if (cells[0][2] == maker && cells[1][1] == maker && cells[2][0] == maker) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,6 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
-=======
-        System.out.println("Hello Pinar!");
->>>>>>> 8a2d5595c7b3996f90dd652062dc2d70b2ca42c0
     }
 }
